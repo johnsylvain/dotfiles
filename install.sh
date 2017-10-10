bdca831d91cb1f3b754d316f133eba442d6b7fbb
John Sylvain <john.n.sylvain@gmail.com>
2017-10-10 15:58:15 -0500

    updated install script

--- install.sh
+++ install.sh
@@ -2,49 +2,21 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-ZSH=(
+FILES=(
+	# ZSH
 	"ln -si ${BASEDIR}/.zshrc ~/.zshrc"
 	"ln -si ${BASEDIR}/john.zsh-theme $HOME/.oh-my-zsh/themes/john.zsh-theme"
-)
-
-VIM=(
+	
+	# Vim
 	"ln -si ${BASEDIR}/.vimrc ~/.vimrc"
-)
-
-GIT=(
+	
+	# Git
 	"ln -si ${BASEDIR}/.gitconfig ~/.gitconfig"
 )
 
-ALL_FILES=("git" "zsh")
 
-install_files() {
-	printf "\n\e[1mInstalling $1 dotfiles...\e[0m\n\n"
-	if [ "$1" == "git" ]; then
-		for i in "${GIT[@]}"; do
-			echo "exec: $i"
-			eval $i
-		done
-	elif [ "$1" == "zsh" ]; then
-		for i in "${ZSH[@]}"; do
-			echo "exec: $i"
-			eval $i
-		done
-	else
-		echo "not found: $1"
-	fi		
-}
-
-install_all() {
-	printf "\n\e[1mInstalling all dotfiles...\e[0m\n"
-	for f in "${ALL_FILES[@]}"; do
-		install_files $f
-	done
-}
-
-if [ -z "$1" ];
-	then
-		install_all
-	else
-		install_files $1
-fi
-
+printf "\n\e[1mInstalling all dotfiles...\e[0m\n\n"
+for f in "${FILES[@]}"; do
+	echo "exec: $f"
+	eval $f
+done
