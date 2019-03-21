b73abf3000796da486cb9c47fbe128ad3b216bdc
John Sylvain <john.n.sylvain@gmail.com>
2019-03-20 19:18:21 -0500

    add homebrew packages

--- install.sh
+++ install.sh
@@ -28,3 +28,16 @@ done
 printf "\n\e[1mInstalling scripts...\e[0m\n\n"
 curl -L https://raw.githubusercontent.com/kamranahmedse/git-standup/master/installer.sh | sudo sh
 curl -L https://raw.githubusercontent.com/johnsylvain/git-cleanup/master/installer.sh | sudo sh
+
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Install packages
+brew install tmux
+brew install thefuck
+brew install zsh-syntax-highlighting
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+curl -s "https://get.sdkman.io" | bash
