a31ae0c75e276a3df97836bac532391614e3919e
John Sylvain <john.n.sylvain@gmail.com>
2019-03-26 18:28:25 -0500

    remove zprofile and add additional git function

--- .zshrc
+++ .zshrc
@@ -3,3 +3,16 @@ export UPDATE_ZSH_DAYS=13
 ZSH_THEME="john"
 source $ZSH/oh-my-zsh.sh
 source $HOME/.oh-my-zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+export SSH_KEY_PATH="~/.ssh/rsa_id"
+
+export PATH="$PATH:/usr/local/mysql/bin"
+export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+export PATH="$HOME/.yarn/bin:$PATH"
+export PATH="$PATH:$HOME/.rvm/bin"
+export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+source "$HOME/.zfunctions"
+source "$HOME/.zaliases"
--- install.sh
+++ install.sh
@@ -8,12 +8,16 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `install.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+if test ! $(which zsh); then
+	printf "\n\e[1mInstalling zsh...\e[0m\n\n"
+	sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
 printf "\n\e[1mLinking dotfiles...\e[0m\n\n"
 
 ln -si $BASEDIR/.zshrc ~/.zshrc
 ln -si $BASEDIR/.zfunctions ~/.zfunctions
 ln -si $BASEDIR/.zaliases ~/.zaliases
-ln -si $BASEDIR/.zprofile ~/.zprofile
 ln -si $BASEDIR/john.zsh-theme $HOME/.oh-my-zsh/themes/john.zsh-theme
 ln -si $BASEDIR/.vimrc ~/.vimrc
 ln -si $BASEDIR/.tmux.conf ~/.tmux.conf
